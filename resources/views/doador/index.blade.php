cc42ed96f1d6b5273356222ee99e928ed4fc6e54
Carlos Tiago <carlostiago@live.com>
2017-08-21 21:27:12 -0300

    ajuste no link CLIQUE AQUI PARA AGENDAR

--- resources/views/doador/index.blade.php
+++ resources/views/doador/index.blade.php
@@ -6,7 +6,7 @@
             <h1>Seja bem vindo!</h1>
             <p>Doar sangue é se preocupar com o próximo. Pratique esse hábito! Existem muitas pessoas que estão
                 precisando urgentemente de doações de sangue, por que não ajudar? </p>
-            <p><a class="btn btn-primary btn-lg">Clique aqui para agendar</a></p>
+            <p><a href="{{ route('doador.agendar') }}" class="btn btn-primary btn-lg">Clique aqui para agendar</a></p>
         </div>
     </div>
 @endsection
